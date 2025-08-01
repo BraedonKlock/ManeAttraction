877bb7647efda2e6003885190df579bb4da53ed8
Braedon Klock <kloc0004@algonquinlive.com>
2025-08-01 09:49:32 -0400

    fixed paths

--- JavaScript/home.js
+++ JavaScript/home.js
@@ -14,7 +14,7 @@
     welcomeMess.innerHTML = `
       Welcome to Mane Attraction Hair Design! I’m Kendra, a passionate stylist specializing in color, cuts, and styles that bring out your natural beauty. 
       <br><br>
-      <img id="home-image" src="../Photos/kendrahome.jpeg" alt="Kendra Headshot">
+      <img id="home-image" src="./Photos/kendrahome.jpeg" alt="Kendra Headshot">
       
       Whether you're preparing for your big day, a special event, or just want a fresh new look, I’m here to make you feel confident and radiant. From bridal and formal styling to everyday transformations, I’m dedicated to bringing your hair goals to life with creativity and care.`;
     main.appendChild(welcomeMess);
